2db9e32923d9c0cc66a8982e0a12c32d20a4502d
brijeshamin25 <brijeshamin1997@gmail.com>
2024-05-26 18:20:41 -0400

    Animation completed

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ window.onscroll = () => {
   //remove toggle icon and navbar when click navbar links (scroll)
   menuIcon.classList.remove('bx-x');
   navBar.classList.remove('active');
+
+  //animation footer on scroll
+  let footer = document.querySelector('footer');
+  footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
 //toggle icon navbar
